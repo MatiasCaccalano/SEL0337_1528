2ad25dcae0378bc9faf84854167bb3315cc4a1f5
MatiasCaccalano <matias.caccalano@usp.br>
2024-11-25 15:47:15 -0300

    Update LEDPWM.py atualizando os comentarios

--- LEDPWM.py
+++ LEDPWM.py
@@ -8,8 +8,8 @@ led = PWMLED(18) #Define o LED que vai ser usado
 
 while True: #Loop infinito
     led.value = 0 #Led desligado
-    sleep(0.1)      #Espera 1 segundo
+    sleep(0.1)      #Espera 0,1 segundo
     led.value = 0.5 #duty cyle em 50%
-    sleep(0.1)      #Espera 1 segundo
+    sleep(0.1)      #Espera 0,1 segundo
     led.value = 1  #duty cyle em 100%
-    sleep(0.1)      #Espera 1 segundo
+    sleep(0.1)      #Espera 0,1 segundo
